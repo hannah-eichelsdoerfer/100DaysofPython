a6359481abf79316e818c191a296c8b7c4ad0708
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-07-25 21:56:21 +0200

    Lessons learned

---
+++ day3.py
@@ -0,0 +1,102 @@
+# (nested) if-else statements, comparison operators (>, <, <=, >=, ==, !=)
+# elif
+# Multiple conditions (multiple if statements in succession)
+print("Welcome to the rollercoaster 🎢")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+  print("You can ride the rollercoaster!")
+  age = int(input("How old are you? "))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5")
+  elif age < 18 :
+    bill = 7
+    print("Youth tickets are $7")
+  elif age > 45 and age < 55:
+    bill = 0
+    print("People your age ride for free!")
+  else:
+    bill = 12
+    print("$12 for an adult ticket")
+  
+  photo = input("Do you want a photo taken? Y/N. ")
+  if photo == "Y":
+    bill += 3
+  print(f"Your final bill is ${bill}")   
+else:
+  print("Sorry, you have to grow taller before you can ride.")
+  
+# Odd or Even Exercise (Modulo Operation)
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+  print("This is an even number")
+else:
+  print("This is an odd number")
+
+# BMI Calculator advanced ⚖
+height = float(input("Enter your height in cm: ")) / 100
+weight = float(input("Enter your weight in kg: "))
+
+bmi = round((weight / (height * height)), 1)
+
+print(f"Your BMI is {bmi}.");
+
+if bmi < 18.5:
+  print("You are underweight")
+elif bmi < 25:
+  print("You have a normal weight")
+elif bmi < 30:
+  print("You are overweight")
+elif bmi < 35:
+  print("You are obwese")
+else:
+  print("You are clinically obese")
+  
+# Leap Year
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap Year!")
+    else:
+      print("Not a leap year!")
+  else:
+    print("Leap year!")
+else:
+  print("Not a leap year!")
+  
+# Pizza Order Program 🍕
+# Logical operators (written out in python)
+# and (equivalent to writing &&)
+# or (equivalent of ||)
+# not
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size do you want your pizza? S, M or L? ")
+add_pepperoni = input("Do you want pepperoni? ")
+cheesy_crust = input("Do you want a cheesy crust? ")
+
+price = 0
+
+if size == "S":
+  price += 15
+elif size == "M":
+  price += 20
+elif size == "L":
+  price += 25
+
+if add_pepperoni == "Y" and size == "S":
+  price += 2
+else:
+  price += 3
+  
+if cheesy_crust == "Y":
+  price += 1
+  
+print(f"Your final bill is ${price}💵")
+
+# Day 3 Project - Treasue Island
