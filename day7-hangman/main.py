14e654a0f1076ed7e279d4f07bf2e8690dca3b44
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-07-27 19:53:29 +0200

    Final Hangman Game

--- day7-hangman/main.py
+++ day7-hangman/main.py
@@ -1,6 +1,17 @@
 import random
 from hangman_words import word_list
 from hangman_ascii_art import logo, stages
+
+from os import system, name
+def clear():
+  # for windows
+  if name == 'nt':
+      _ = system('cls')
+
+  # for mac and linux(here, os.name is 'posix')
+  else:
+      _ = system('clear')
+        
 # Day 7 Project - Hangman
 
 # generate a random word -> fill letters with blanks
@@ -9,6 +20,7 @@ from hangman_ascii_art import logo, stages
 # else start drawing hangman
 # this will be repeated till hanged or word guessed (while loop?)
 
+clear()
 print(logo)
 
 display = []
@@ -24,22 +36,28 @@ end_of_game = False
 hanged = 0
 
 while not end_of_game:
-  guess = input("Guess a letter: \n").lower()
+  guess = input("Guess a letter: ").lower()
+  
+  clear()
+  
+  if guess in display:
+    print("You've already guessed this letter correctly!")
   
   for number in range(word_lenght):
     letter = chosen_word[number]
     if letter == guess:
+      print(f"Correct, the letter {guess} is in the secret word!")
       display[number] = guess
   
   if guess not in chosen_word:
     hanged += 1
-    print(f"Sorry, the letter {letter} is not in the secret word, the hanging continues...")
-    print(stages[hanged])
+    print(f"Sorry, the letter {guess} is not in the secret word, the hanging continues...")
     if hanged == 6:
+      print(f"You lost 💀💀💀 - the secret word was {chosen_word}")
       end_of_game = True
-      print("You lost 💀💀💀")
   
   print(" ".join(display))
+  print(stages[hanged])
   
   if "_" not in display:
     end_of_game = True
